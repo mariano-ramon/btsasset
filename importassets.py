70aa36fdffbda2d07731d2d081af228e20d63fe2
Mariano Ramon <marianog.ramon@gmail.com>
2018-01-22 11:04:50 -0300

    gets variance of producer from median for last results

--- importassets.py
+++ importassets.py
@@ -1,18 +1,33 @@
+from itertools import chain
+from datetime import datetime
+from statistics import median
+
 from bitshares.asset import Asset
 
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-from json import loads
+from sqlalchemy.sql import text
 
-engine = create_engine('sqlite:///./assets.db', echo=True)
+engine = create_engine('sqlite:///./assets.db', echo=False)
 Base = declarative_base()
 
 Session = sessionmaker()
 Session.configure(bind=engine)
 db = Session()
+
+
+
+def execsql(stmt="", **kwargs):
+    plain = []
+    with engine.connect() as conn:
+        result = conn.execute(text(stmt), **kwargs)
+        for r in result:
+            plain.append(list(r))
+
+    return plain
+
 
 class BTSAsset(Base):
 
@@ -61,10 +76,67 @@ class BlockchainDB:
 
         db.commit()
 
+    @staticmethod
+    def get_last_prices(asset="USD"):
+        r = execsql("select prices.sp from bts_asset as prices "
+                        "inner join (select max(timestamp) ts, producer from bts_asset group by producer, asset) last "
+                        "on prices.producer = last.producer "
+                        "and prices.timestamp = last.ts "
+                        "where prices.asset = :asset", asset=asset)
+
+        return list(chain(*r))
+
+    @staticmethod
+    def get_all_producers():
+        r = execsql("select distinct(producer) from bts_asset")
+        return list(chain(*r))
+
+
+    @staticmethod
+    def get_last_median(asset="USD"):
+        r = BlockchainDB.get_last_prices(asset)
+        return median(r)
+
+    @staticmethod
+    def get_variance_of(producers="ALL", asset="USD"):
+        
+        if producers == "ALL":
+            prods = BlockchainDB.get_all_producers()
+        else:
+            prods = producers
+
+        r = execsql("select prices.producer, prices.sp from bts_asset as prices "
+                    "inner join (select max(timestamp) ts, producer from bts_asset group by producer, asset) last "
+                    "on prices.producer = last.producer "
+                    "and prices.timestamp = last.ts "
+                    "where prices.producer in ({})".format(",".join(map(repr, prods))))
+
+        medval = BlockchainDB.get_last_median()
+        for prod,price in r:
+            print("{}: {}%".format(prod, BlockchainDB.get_variance(medval, price)))
+
+
+
+    @staticmethod
+    def get_variance(medval,prodprice):
+
+        variance = 0
+
+        if medval < prodprice:
+            variance = (prodprice - medval) / prodprice * 100 
+
+        if medval > prodprice:
+            variance = ((medval - prodprice) / prodprice * 100 ) * -1
+
+        return variance
+
 
 if __name__ == '__main__':
     bdb = BlockchainDB()
-    bdb.get_assets()
+    # bdb.get_assets()
+
+    #print(bdb.get_last_median())
+    bdb.get_variance_of()
 
     # use this once to create db
     #Base.metadata.create_all(engine)
